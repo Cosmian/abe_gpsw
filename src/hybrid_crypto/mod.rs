4fe4fd553bfe91e9a599724e8a7edecc51a95359
Manuthor <32013169+Manuthor@users.noreply.github.com>
2022-01-14 11:01:45 +0100

    Update src/hybrid_crypto/mod.rs
    Co-authored-by: Thibs <ThibsG@users.noreply.github.com>

--- src/hybrid_crypto/mod.rs
+++ src/hybrid_crypto/mod.rs
@@ -157,7 +157,7 @@ pub fn generate_symmetric_key_and_header(
 /// - `first_block_number` is also part on the AES AEAD and is increased on
 ///   every AES block (4096 bytes)
 ///
-/// Returns the encrypted data (pre-pended with the header) ant the last AES
+/// Returns the encrypted data (pre-pended with the header) and the last AES
 /// block number
 pub fn encrypt(
     symmetric_key: &[u8],
